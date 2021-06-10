9e1bf0e48329937268a89ecf57372469c9c88d4e
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2021-06-10 17:48:20 +0900

    Add comments to GameComponent.tsx

--- src/GameComponent.tsx
+++ src/GameComponent.tsx
@@ -28,18 +28,19 @@ export default class GameComponent extends Component<{}, GameState> {
     );
     // 初期値の設定
     this.state = {
-      cards: this.table.arr,
-      status: this.table.sts,
-      ready: -1,
-      message: "",
-      count: 15,
-      timer: 0,
-      title: "",
-      run: false,
-      overlay: "overlay",
+      cards: this.table.arr, //絵柄 （"♡"や"♧"など）の配列
+      status: this.table.sts, //カードの状態を表す配列
+      ready: -1, //クリックされたカードの状態
+      message: "", //判定後のメッセージ
+      count: 15, //カウントダウンの数値
+      timer: 0, //setInterval()の戻り値を格納
+      title: "", //画面中央に表示する文言
+      run: false, //ゲームの状態
+      overlay: "overlay", //オーバーレイのクラス指定
     };
   }
 
+  // カードを描画する関数
   renderCard(i: number) {
     return (
       <Card
@@ -53,6 +54,7 @@ export default class GameComponent extends Component<{}, GameState> {
   render() {
     const cards = [];
     for (let i = 0; i < 10; i++) {
+      // カード配列にrenderCard(i)で返ってくるJSXを代入する
       cards.push(this.renderCard(i));
     }
     return (
@@ -76,14 +78,18 @@ export default class GameComponent extends Component<{}, GameState> {
     if (this.state.run) {
       return;
     }
+    // １秒ごとにカウントダウン関数を呼ぶ
     const timer = window.setInterval(() => {
       this.countDown();
     }, 1000);
+    // state更新
     this.setState({ timer: timer, run: true, overlay: "" });
   }
 
+  // ゲームスタートボタンが押された後１秒ごとに呼ばれるカウントダウンメソッド
   private countDown(): void {
     let nextCount = this.state.count - 1;
+    // カウントが0になった時
     if (nextCount < 1) {
       this.setState({
         message: "",
@@ -102,13 +108,14 @@ export default class GameComponent extends Component<{}, GameState> {
 
   // カードがクリックされた時に呼ばれる関数
   private cardClicked(i: number): void {
+    // this.state.statusをコピー　更新したstateを保持
     const sts: number[] = this.state.status.slice();
     //未選択以外をクリックされた時は無視
     if (this.state.status[i] !== 0) {
       return;
     }
-    // ゲームスタートしてなければ無視
     let run = this.state.run;
+    // ゲームスタートしてなければ無視
     if (!run) {
       return;
     }
@@ -152,6 +159,7 @@ export default class GameComponent extends Component<{}, GameState> {
         sts[this.state.ready] = 3;
         sts[i] = 3;
         const rollbacksts = this.state.status.slice();
+        // 元の状態(0で埋められた配列)に戻す
         rollbacksts[this.state.ready] = 0;
         rollbacksts[i] = 0;
         setTimeout(() => {
@@ -175,10 +183,12 @@ export default class GameComponent extends Component<{}, GameState> {
     this.setState({ message: "" });
   }
 
+  // 二枚の絵柄が違った場合初期状態に戻す
   private cardReset(sts: number[]): void {
     this.setState({ status: sts, ready: -1 });
   }
 
+  // ゲームが終了するかどうか判定する
   private isFinish(status: number[]): boolean {
     let flg = true;
     for (let i = 0; i < status.length; i++) {
