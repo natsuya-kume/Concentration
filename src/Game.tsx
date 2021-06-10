ff406966623cdf1df3e7884040ea222be25b90b5
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2021-06-10 16:42:06 +0900

    Modify some files

---
+++ src/Game.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import Table from "./Table";
+import GameComponent from "./GameComponent";
+import "./Concentration.css";
+
+const Game = () => {
+  return (
+    <div className="game">
+      <div className="main">
+        <GameComponent />
+      </div>
+    </div>
+  );
+};
+
+export default Game;
